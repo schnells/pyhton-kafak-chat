ef91da52e7b5ab4763376a1dd1646c5b5be71d8d
Simon Schnell <symoN@live.de>
2022-02-06 17:30:45 +0100

    Updated consumer.py

--- consumer.py
+++ consumer.py
@@ -1,10 +1,13 @@
 from pykafka import KafkaClient
+from pykafka.common import OffsetType
 
 client = KafkaClient(hosts="127.0.0.1:9092")
 
 topic = client.topics['chatroom1']
 
-consumer = topic.get_simple_consumer(consumer_group="mychat")
+consumer = topic.get_simple_consumer(consumer_group="mychat",
+    auto_offset_reset=OffsetType.EARLIEST,
+    reset_offset_on_start=False)
 for message in consumer:
     if message is not None:
         print(message.value)
